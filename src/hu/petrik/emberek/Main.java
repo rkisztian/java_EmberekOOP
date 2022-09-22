e499af3fa4f7d8aca51bba314a0f80a35cbf469c
Krisztián <radeczki.krisztian@gmail.com>
2022-09-22 16:05:17 +0200

    Az ember osztályban elkészültek az adattagok toString Mainben 3 embert létrehoztunk és kiisírtunk

--- src/hu/petrik/emberek/Main.java
+++ src/hu/petrik/emberek/Main.java
@@ -1,7 +1,15 @@
 package hu.petrik.emberek;
 
+
+
 public class Main {
     public static void main(String[] args){
+        Ember e1 = new Ember("Gipsz Jakab", "1998-1-12", "Budapest");
+        Ember e2 = new Ember("Teszt Elek", "2003-12-5", "Debrecen");
+        Ember e3 = new Ember("Kovács István", "1973-9-22", "Pécs");
 
+        System.out.println(e1);
+        System.out.println(e2);
+        System.out.println(e3);
     }
 }
